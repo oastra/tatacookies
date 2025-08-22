c78e88eb366c444428af39b0ab36c199bfc052cb
Olha Chernysh <oastra555@gmail.com>
2025-08-22 14:07:55 +1000

    Fix footer

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,6 +10,7 @@ import TestimonialsSection from "@/components/sections/TestimonialsSection";
 import BlogSection from "@/components/sections/BolgSection";
 import ContactSection from "@/components/sections/ContactSection";
 import dynamic from "next/dynamic";
+import Footer from "@/components/common/Footer";
 
 // Keep only truly browser-dependent
 const InstagramSection = dynamic(
@@ -37,6 +38,7 @@ export default function Home() {
 
         <ContactSection />
       </main>
+      <Footer />
     </>
   );
 }
